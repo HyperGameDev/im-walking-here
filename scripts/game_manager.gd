7cd292ac4fbda716e6bf1e468631ad0845d862e9
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2025-06-20 19:34:22 -0400

    audio manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,13 @@
 extends Node
 
 var score : float = 0
-var score_mult : float = 1.
+var score_mult : float = 1.:
+	get:
+		return score_mult
+	set(val):
+		if score_mult != val:
+			score_mult = val
+		score_mult = clampf(score_mult, 0.1, 2)
 
 func reset_vals() -> void:
 	score = 0
