e498ee68290639b38a57bd8e3e5e98f0d29914bb
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2025-06-20 21:25:15 -0500

    UI and score updates

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,21 +1,23 @@
 extends Node
 
-var score : float = 0
-var score_mult : float = 1.:
-	get:
-		return score_mult
-	set(val):
-		if score_mult != val:
-			score_mult = val
-		score_mult = clampf(score_mult, 0.1, 2)
+var score : int = 0
+var score_mult : float = 10.
+	#get:
+		#return score_mult
+	#set(val):
+		#if score_mult != val:
+			#if score_mult > 0.:
+				#score_mult = val
+		##score_mult = clampf(score_mult, 0.1, 2)
+		
+func restart_game() -> void:
+	reset_vals()
 
 func reset_vals() -> void:
 	score = 0
-	score_mult = 1.
+	#score_mult = 1.
 
 func finish_round() -> void:
 	if score > SaveGame.Highscore:
 		SaveGame.Highscore = score
 		SaveGame.save_game()
-	
-	reset_vals()
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -10,9 +10,16 @@ func _init() -> void:
 	ref = self
 
 func _process(delta: float) -> void:
+	label_score.text = str(GameManager.score)
+	
 	if Path.ref.round_length > 0.:
 		var minutes = int(Path.ref.round_length) / 60
 		var seconds = int(Path.ref.round_length) % 60
 		label_time.text = "%02d:%02d" % [minutes, seconds]
 	else:
 		GameManager.finish_round()
+		
+func update_score() -> void:
+	var new_score: int = GameManager.score + progress_happiness.value * GameManager.score_mult
+	var tween: Tween = create_tween()
+	tween.tween_property(GameManager,"score",new_score,.2)
