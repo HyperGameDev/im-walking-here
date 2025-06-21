c35e4aaac027a9bf8f23da2e217c58c2b252d95f
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2025-06-21 01:46:38 -0400

    clickable frog

--- scripts/frog.gd
+++ scripts/frog.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 var time : float
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _process(delta: float) -> void:
 	time += delta
-	var x_position = global_position.x
-	var y_position = sin(time * 3) * 1
+	var x_position : float = global_position.x
+	var y_position : float = sin(time * 3) * 1
 	
 	global_position = Vector2(x_position, global_position.y + y_position)
-	pass
+
+func _on_item_clicked() -> void:
+	audio_stream_player_2d.play()
+	
